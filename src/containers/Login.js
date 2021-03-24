9d2d7593396f828628214e0111f1fcdbe23e5b86
wenhua <yao.wen@husky.neu.edu>
2021-03-24 15:25:47 -0700

    added intermediate effect of logging in

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 import { useAppContext } from "../libs/contextLib";
@@ -11,6 +11,7 @@ export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
 
     function validateForm() {
         return email.length > 0 && password.length > 0;
@@ -19,12 +20,15 @@ export default function Login() {
     async function handleSubmit(event) {
         event.preventDefault();
 
+        setIsLoading(true);
+
         try {
             await Auth.signIn(email, password);
             userHasAuthenticated(true);
             history.push("/");
         } catch (e) {
             alert(e.message);
+            setIsLoading(false);
         }
     }
 
@@ -48,9 +52,15 @@ export default function Login() {
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </Form.Group>
-                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                <LoaderButton
+                    block
+                    size="lg"
+                    type="submit"
+                    isLoading={isLoading}
+                    disabled={!validateForm()}
+                >
                     Login
-        </Button>
+                </LoaderButton>
             </Form>
         </div>
     );
