0e6f10c8673104537fb6f13f935d7e2346677fcb
wenhua <yao.wen@husky.neu.edu>
2021-03-28 13:40:42 -0700

    Use the Redirect Routes

--- src/Routes.js
+++ src/Routes.js
@@ -7,28 +7,27 @@ import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
 import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
     return (
         <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route exact path="/login">
+            <UnauthenticatedRoute exact path="/login">
                 <Login />
-            </Route>
-            <Route exact path="/signup">
+            </UnauthenticatedRoute>
+            <UnauthenticatedRoute exact path="/signup">
                 <Signup />
-            </Route>
-            <Route exact path="/settings">
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute exact path="/settings">
                 <Settings />
-            </Route>
-            <Route exact path="/notes/new">
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/new">
                 <NewNote />
-            </Route>
-            <Route exact path="/notes/:id">
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/:id">
                 <Notes />
-            </Route>
+            </AuthenticatedRoute>
             {/* Finally, catch all unmatched routes */}
             <Route>
                 <NotFound />
