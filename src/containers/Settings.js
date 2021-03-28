5d423f9d037c489fbf5eec08af1ec9c9e2ecb45f
wenhua <yao.wen@husky.neu.edu>
2021-03-28 13:34:50 -0700

    Connect the Billing Form

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -3,6 +3,9 @@ import { API } from "aws-amplify";
 import { useHistory } from "react-router-dom";
 import { onError } from "../libs/errorLib";
 import config from "../config";
+import { Elements, StripeProvider } from "react-stripe-elements";
+import BillingForm from "../components/BillingForm";
+import "./Settings.css";
 
 export default function Settings() {
     const history = useHistory();
@@ -19,8 +22,42 @@ export default function Settings() {
         });
     }
 
+    async function handleFormSubmit(storage, { token, error }) {
+        if (error) {
+            onError(error);
+            return;
+        }
+
+        setIsLoading(true);
+
+        try {
+            await billUser({
+                storage,
+                source: token.id
+            });
+
+            alert("Your card has been charged successfully!");
+            history.push("/");
+        } catch (e) {
+            onError(e);
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div className="Settings">
+            <StripeProvider stripe={stripe}>
+                <Elements
+                    fonts={[
+                        {
+                            cssSrc:
+                                "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800",
+                        },
+                    ]}
+                >
+                    <BillingForm isLoading={isLoading} onSubmit={handleFormSubmit} />
+                </Elements>
+            </StripeProvider>
         </div>
     );
 }
